3f1ce0d117b93ea0699ccb0cd63c6f72fab10c31
Nikki <71370291+13564190@users.noreply.github.com>
2021-05-08 14:10:28 +1000

    Worked on implementing shooting enemies
    - Added new fire sprite
    - Added to enemy script
    - Added flashing and explosion effect

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,14 +8,57 @@ public class Enemy : MonoBehaviour
 
     public float enemySpeed;
 
+    private int health = 3;
+
+    //Materials
+    private Material matWhite;
+    private Material matDefault;
+    private UnityEngine.Object explosionRef;
+    SpriteRenderer sr;
+
     void Start()
     {
         enemy = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
+        matDefault = sr.material;
+        explosionRef = Resources.Load("Explosion");
+
     }
 
     void Update()
     {
         Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            health--;
+            sr.material = matWhite;
+            if(health <= 0)
+            {
+                KillSelf();
+            }
+            else
+            {
+                Invoke("ResetMaterial", .1f);
+            }
+        }
+    }
+
+    void ResetMaterial()
+    {
+        sr.material = matDefault;
+    }    
+
+    private void KillSelf()
+    {
+        GameObject explosion = (GameObject)Instantiate(explosionRef);
+        explosion.transform.position = new Vector3(transform.position.x, transform.position.y + .3f, transform.position.z);
+        Destroy(gameObject);
     }
 
     void Move()
